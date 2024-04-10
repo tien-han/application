00647a7a59898717f460f615a9f93dd09c707ec8
Tien Han <tienthuyhan@gmail.com>
2024-04-09 20:32:38 -0400

    Add composer file and set up default view

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 328/application/index.php
+ * @author Tien Han
+ * @version 1.0
+ */
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Instantiate the F3 Base class (Fat-Free)
+$f3 = Base::instance();
+
+//Define a default route
+$f3-> route('GET /', function() {
+    //Render a view page
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+//Run Fat-Free
+$f3->run();
+?>
