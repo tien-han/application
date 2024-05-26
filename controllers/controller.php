bb5e09cbb6e61bbdcaf40034e77d38308afcb834
Tien Han <tienthuyhan@gmail.com>
2024-05-26 04:16:23 -0400

    Update route code in index to controller methods

---
+++ controllers/controller.php
@@ -0,0 +1,250 @@
+<?php
+    /**
+     * @description: This is the Controller class that defines a method for each route.
+     * @author: Tien Han
+     * @date: 5/26/2024
+     */
+
+    class Controller
+    {
+        private $_f3; //Fat-Free Router
+
+        function __construct($f3)
+        {
+            $this->_f3 = $f3;
+        }
+
+        function home(): void
+        {
+            //Render a view page
+            $view = new Template();
+            echo $view->render('views/home.html');
+        }
+
+        function personalInfoForm(): void
+        {
+            //If the user has submitted a post request (i.e. filled out the form)
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                //Get submitted form data
+                $firstName = $_POST['firstName'];
+                $lastName = $_POST['lastName'];
+                $email = $_POST['email'];
+                $state = $_POST['state'];
+                $phone = $_POST['phone'];
+
+                $allValid = true;
+
+                //Perform validation on the submitted first name
+                $firstNameError = '';
+                if (!empty($firstName)) {
+                    if (!validName($firstName)) {
+                        $allValid = false;
+                        $firstNameError = 'Please enter in an alphabetic first name!';
+                    }
+                } else {
+                    //First name is required
+                    $allValid = false;
+                    $firstNameError = 'Please enter your first name.';
+                }
+                $this->_f3->set('SESSION.firstName', $firstName);
+                $this->_f3->set('SESSION.firstNameError', $firstNameError);
+
+                //Perform validation on the submitted last name
+                $lastNameError = '';
+                if (!empty($lastName)) {
+                    if (!validName($lastName)) {
+                        $allValid = false;
+                        $lastNameError = 'Please enter in an alphabetic last name!';
+                    }
+                } else {
+                    //Last name is required
+                    $allValid = false;
+                    $lastNameError = 'Please enter your last name.';
+                }
+                $this->_f3->set('SESSION.lastName', $lastName);
+                $this->_f3->set('SESSION.lastNameError', $lastNameError);
+
+                //Perform validation on the submitted email
+                $emailError = '';
+                if (!empty($email)) {
+                    if (!validEmail($email)) {
+                        $allValid = false;
+                        $emailError = 'Please enter in a valid email!';
+                    }
+                } else {
+                    //Email is required
+                    $allValid = false;
+                    $emailError = 'Please enter an email.';
+                }
+                $this->_f3->set('SESSION.email', $email);
+                $this->_f3->set('SESSION.emailError', $emailError);
+
+                //Perform validation on the submitted state
+                $stateError = '';
+                if (!empty($state)) {
+                    if (!validState($state)) {
+                        $allValid = false;
+                        $stateError = 'Please select a valid state or no state!';
+                    }
+                }
+                $this->_f3->set('SESSION.state', $state);
+                $this->_f3->set('SESSION.stateError', $stateError);
+
+                //Perform validation on the submitted phone number
+                $phoneError = '';
+                if (!empty($phone)) {
+                    if (!validPhone($phone)) {
+                        $allValid = false;
+                        $phoneError = 'Please enter in a valid phone number!';
+                    }
+                } else {
+                    //Phone number is required
+                    $allValid = false;
+                    $phoneError = 'Please enter a phone number.';
+                }
+                $this->_f3->set('SESSION.phone', $phone);
+                $this->_f3->set('SESSION.phoneError', $phoneError);
+
+                //Record whether the applicant wants to sign up for mailing lists or not
+                if (isset($_POST['optInML'])) {
+                    $this->_f3->set('SESSION.optInML', true);
+                } else {
+                    $this->_f3->set('SESSION.optInML', false);
+                }
+
+                //Redirect to the experience form page
+                if ($allValid) {
+                    $this->_f3->reroute("application-form/experience");
+                }
+            }
+
+            //Get all the states to populate the job application
+            $this->_f3->set('states', getStates());
+
+            //Render a view page
+            $view = new Template();
+            echo $view->render('views/form-personal-information.html');
+        }
+
+        function experienceForm(): void
+        {
+            //If the user has submitted a post request (i.e. filled out the form)
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                //Get submitted form data
+                $biography = $_POST['biography'];
+                $github = $_POST['github'];
+                $experience = $_POST['years_experience'];
+                $relocate = $_POST['relocate'];
+
+                $allValid = true;
+
+                //Save any text entered for biography
+                if (!empty($biography)) {
+                    $this->_f3->set('SESSION.biography', $biography);
+                }
+
+                //Perform validation on the submitted GitHub Link if any was submitted
+                $githubError = '';
+                if (!empty($github)) {
+                    if (!validGitHub($github)) {
+                        $allValid = false;
+                        $githubError = 'Please enter in a valid URL!';
+                    }
+                    $this->_f3->set('SESSION.github', $github);
+                    $this->_f3->set('SESSION.githubError', $githubError);
+                }
+
+                //Perform validation on the submitted Years of Experience
+                $yearsExperienceError = '';
+                if (!empty($experience)) {
+                    if (!validExperience($experience)) {
+                        $allValid = false;
+                        $yearsExperienceError = 'Please select one of the years of experience!';
+                    }
+                } else {
+                    //Years of experience is required
+                    $allValid = false;
+                    $yearsExperienceError = 'Please select your years of experience.';
+                }
+                $this->_f3->set('SESSION.experience', $experience);
+                $this->_f3->set('SESSION.experienceError', $yearsExperienceError);
+
+                //Perform validation on the submitted relocation selection if any was submitted
+                $relocationError = '';
+                if (!empty($relocate)) {
+                    if (!validRelocation($relocate)) {
+                        $allValid = false;
+                        $relocationError = 'Please select one of the options for Willing to Relocate!';
+                    }
+                    $this->_f3->set('SESSION.relocate', $relocate);
+                    $this->_f3->set('SESSION.relocationError', $relocationError);
+                }
+
+                if ($allValid) {
+                    //Check to see if the user would like to see mailing lists
+                    if ($this->_f3->get('SESSION.optInML')) {
+                        //Redirect to the mailing list
+                        $this->_f3->reroute("application-form/mailing-list");
+                    }
+
+                    //If the user isn't interested in any mailing lists
+                    //Redirect to the summary page
+                    $this->_f3->reroute("application-form/summary");
+                }
+            }
+
+            //Get all the years of experience to populate the job application
+            $this->_f3->set('yearsExperience', getYearsExperience());
+
+            //Get all the options for willing to relocate to populate the job application
+            $this->_f3->set('willingToRelocate', getRelocation());
+
+            //Render a view page
+            $view = new Template();
+            echo $view->render('views/form-experience.html');
+        }
+
+        function mailingListForm(): void
+        {
+            //If the user has submitted a post request (i.e. filled out the form)
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $jobAndMailingLists = '';
+                $mailing = '';
+
+                //Get submitted form data
+                if ($_POST['job'] != null and $_POST['vertical'] != null) {
+                    $jobAndMailingLists = array_merge($_POST['job'], $_POST['vertical']);
+                } else if ($_POST['job'] != null) {
+                    $jobAndMailingLists = $_POST['job'];
+                } else if ($_POST['vertical'] != null) {
+                    $jobAndMailingLists = $_POST['vertical'];
+                }
+
+                if (gettype($jobAndMailingLists) == 'array') {
+                    $mailing = implode(", ", $jobAndMailingLists);
+                }
+
+                //TODO: Validate that the selected items in the checkboxes is valid (in our list)
+
+                //Save any values that have been entered
+                $this->_f3->set('SESSION.mailing', $mailing);
+
+                //Redirect to the summary page
+                $this->_f3->reroute("application-form/summary");
+            }
+
+            //Get all mailing list items
+            $this->_f3->set('mailingList', getMailingLists());
+
+            //Render a view page
+            $view = new Template();
+            echo $view->render('views/mailing-list.html');
+        }
+
+        function summary(): void
+        {
+            //Render a view page
+            $view = new Template();
+            echo $view->render('views/summary-page.html');
+        }
+    }
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    /*
+    /**
      * 328/application/index.php
      * @author Tien Han
      * @date 5/5/2024
@@ -21,239 +21,31 @@
 
     //Instantiate the F3 Base class (Fat-Free)
     $f3 = Base::instance();
+    $controller = new Controller($f3);
 
     //Define a default route
     $f3->route('GET /', function() {
-        //Render a view page
-        $view = new Template();
-        echo $view->render('views/home.html');
+        $GLOBALS['controller']->home();
     });
 
     //Our route to the form for gathering personal information
-    $f3->route('GET|POST /application-form/personal-information', function($f3) {
-        //If the user has submitted a post request (i.e. filled out the form)
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            //Get submitted form data
-            $firstName = $_POST['firstName'];
-            $lastName = $_POST['lastName'];
-            $email = $_POST['email'];
-            $state = $_POST['state'];
-            $phone = $_POST['phone'];
-
-            $allValid = true;
-
-            //Perform validation on the submitted first name
-            $firstNameError = '';
-            if (!empty($firstName)) {
-                if (!validName($firstName)) {
-                    $allValid = false;
-                    $firstNameError = 'Please enter in an alphabetic first name!';
-                }
-            } else {
-                //First name is required
-                $allValid = false;
-                $firstNameError = 'Please enter your first name.';
-            }
-            $f3->set('SESSION.firstName', $firstName);
-            $f3->set('SESSION.firstNameError', $firstNameError);
-
-            //Perform validation on the submitted last name
-            $lastNameError = '';
-            if (!empty($lastName)) {
-                if (!validName($lastName)) {
-                    $allValid = false;
-                    $lastNameError = 'Please enter in an alphabetic last name!';
-                }
-            } else {
-                //Last name is required
-                $allValid = false;
-                $lastNameError = 'Please enter your last name.';
-            }
-            $f3->set('SESSION.lastName', $lastName);
-            $f3->set('SESSION.lastNameError', $lastNameError);
-
-            //Perform validation on the submitted email
-            $emailError = '';
-            if (!empty($email)) {
-                if (!validEmail($email)) {
-                    $allValid = false;
-                    $emailError = 'Please enter in a valid email!';
-                }
-            } else {
-                //Email is required
-                $allValid = false;
-                $emailError = 'Please enter an email.';
-            }
-            $f3->set('SESSION.email', $email);
-            $f3->set('SESSION.emailError', $emailError);
-
-            //Perform validation on the submitted state
-            $stateError = '';
-            if (!empty($state)) {
-                if (!validState($state)) {
-                    $allValid = false;
-                    $stateError = 'Please select a valid state or no state!';
-                }
-            }
-            $f3->set('SESSION.state', $state);
-            $f3->set('SESSION.stateError', $stateError);
-
-            //Perform validation on the submitted phone number
-            $phoneError = '';
-            if (!empty($phone)) {
-                if (!validPhone($phone)) {
-                    $allValid = false;
-                    $phoneError = 'Please enter in a valid phone number!';
-                }
-            } else {
-                //Phone number is required
-                $allValid = false;
-                $phoneError = 'Please enter a phone number.';
-            }
-            $f3->set('SESSION.phone', $phone);
-            $f3->set('SESSION.phoneError', $phoneError);
-
-            //Record whether the applicant wants to sign up for mailing lists or not
-            if (isset($_POST['optInML'])) {
-                $f3->set('SESSION.optInML', true);
-            } else {
-                $f3->set('SESSION.optInML', false);
-            }
-
-            //Redirect to the experience form page
-            if ($allValid) {
-                $f3->reroute("application-form/experience");
-            }
-        }
-
-        //Get all the states to populate the job application
-        $f3->set('states', getStates());
-
-        //Render a view page
-        $view = new Template();
-        echo $view->render('views/form-personal-information.html');
+    $f3->route('GET|POST /application-form/personal-information', function() {
+        $GLOBALS['controller']->personalInfoForm();
     });
 
     //The form for gathering experience
-    $f3->route('GET|POST /application-form/experience', function($f3) {
-        //If the user has submitted a post request (i.e. filled out the form)
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            //Get submitted form data
-            $biography = $_POST['biography'];
-            $github = $_POST['github'];
-            $experience = $_POST['years_experience'];
-            $relocate = $_POST['relocate'];
-
-            $allValid = true;
-
-            //Save any text entered for biography
-            if (!empty($biography)) {
-                $f3->set('SESSION.biography', $biography);
-            }
-
-            //Perform validation on the submitted GitHub Link if any was submitted
-            $githubError = '';
-            if (!empty($github)) {
-                if (!validGitHub($github)) {
-                    $allValid = false;
-                    $githubError = 'Please enter in a valid URL!';
-                }
-                $f3->set('SESSION.github', $github);
-                $f3->set('SESSION.githubError', $githubError);
-            }
-
-            //Perform validation on the submitted Years of Experience
-            $yearsExperienceError = '';
-            if (!empty($experience)) {
-                if (!validExperience($experience)) {
-                    $allValid = false;
-                    $yearsExperienceError = 'Please select one of the years of experience!';
-                }
-            } else {
-                //Years of experience is required
-                $allValid = false;
-                $yearsExperienceError = 'Please select your years of experience.';
-            }
-            $f3->set('SESSION.experience', $experience);
-            $f3->set('SESSION.experienceError', $yearsExperienceError);
-
-            //Perform validation on the submitted relocation selection if any was submitted
-            $relocationError = '';
-            if (!empty($relocate)) {
-                if (!validRelocation($relocate)) {
-                    $allValid = false;
-                    $relocationError = 'Please select one of the options for Willing to Relocate!';
-                }
-                $f3->set('SESSION.relocate', $relocate);
-                $f3->set('SESSION.relocationError', $relocationError);
-            }
-
-            if ($allValid) {
-                //Check to see if the user would like to see mailing lists
-                if ($f3->get('SESSION.optInML')) {
-                    //Redirect to the mailing list
-                    $f3->reroute("application-form/mailing-list");
-                }
-
-                //If the user isn't interested in any mailing lists
-                //Redirect to the summary page
-                $f3->reroute("application-form/summary");
-            }
-        }
-
-        //Get all the years of experience to populate the job application
-        $f3->set('yearsExperience', getYearsExperience());
-
-        //Get all the options for willing to relocate to populate the job application
-        $f3->set('willingToRelocate', getRelocation());
-
-        //Render a view page
-        $view = new Template();
-        echo $view->render('views/form-experience.html');
+    $f3->route('GET|POST /application-form/experience', function() {
+        $GLOBALS['controller']->experienceForm();
     });
 
     //The form for the mailing list
-    $f3->route('GET|POST /application-form/mailing-list', function($f3) {
-        //If the user has submitted a post request (i.e. filled out the form)
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $jobAndMailingLists = '';
-            $mailing = '';
-
-            //Get submitted form data
-            if ($_POST['job'] != null and $_POST['vertical'] != null) {
-                $jobAndMailingLists = array_merge($_POST['job'], $_POST['vertical']);
-            } else if ($_POST['job'] != null) {
-                $jobAndMailingLists = $_POST['job'];
-            } else if ($_POST['vertical'] != null) {
-                $jobAndMailingLists = $_POST['vertical'];
-            }
-
-            if (gettype($jobAndMailingLists) == 'array') {
-                $mailing = implode(", ", $jobAndMailingLists);
-            }
-
-            //TODO: Validate that the selected items in the checkboxes is valid (in our list)
-
-            //Save any values that have been entered
-            $f3->set('SESSION.mailing', $mailing);
-
-            //Redirect to the summary page
-            $f3->reroute("application-form/summary");
-        }
-
-        //Get all mailing list items
-        $f3->set('mailingList', getMailingLists());
-
-        //Render a view page
-        $view = new Template();
-        echo $view->render('views/mailing-list.html');
+    $f3->route('GET|POST /application-form/mailing-list', function() {
+        $GLOBALS['controller']->mailingListForm();
     });
 
     //The Summary Page
     $f3->route('GET|POST /application-form/summary', function($f3) {
-        //Render a view page
-        $view = new Template();
-        echo $view->render('views/summary-page.html');
+        $GLOBALS['controller']->summary();
     });
 
     //Run Fat-Free
